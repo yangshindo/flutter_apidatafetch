f85703b9bcfeb3455c17aede1c7b610a1f408521
yangshindo <eliwood182@hotmail.com>
2022-05-24 17:42:03 -0300

    my first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
